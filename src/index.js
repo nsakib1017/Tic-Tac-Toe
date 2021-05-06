5baf03f1deb900c7b6948ad2660023827639906c
NAZMUS SAKIB <sakib@priyo.com>
2021-05-06 13:21:27 +0600

    Square is now a Function Component

--- src/index.js
+++ src/index.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-class Square extends React.Component {
-    constructor(props)
-    {
-        super(props);
-        this.state={
-            value: null,
-        };
-    }
-    render() {
-      return (
-        <button 
-            className="square" 
-            onClick={()=>
-            {this.props.onClick()}}
-        >
-          {this.props.value}
-        </button>
-      );
-    }
+// class Square extends React.Component {
+//     constructor(props)
+//     {
+//         super(props);
+//         this.state={
+//             value: null,
+//         };
+//     }
+//     render() {
+//       return (
+//         <button 
+//             className="square" 
+//             onClick={()=>{this.props.onClick()}}
+//         >
+//           {this.props.value}
+//         </button>
+//       );
+//     }
+//   }
+function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
   }
-  
   class Board extends React.Component {
     constructor(props)
     {
@@ -29,6 +34,11 @@ class Square extends React.Component {
         this.state={
             squares: Array(9).fill(null),
         }
+    }
+    handleClick(i){
+        const squares=this.state.squares.slice();
+        squares[i]='X';
+        this.setState({squares: squares});
     }
     renderSquare(i) {
       return (
